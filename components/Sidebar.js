690157fba2a4241f75f247f53b73a6a7b83bf93e
Greg Leeper <gregleeper@Greg-Leeper-Hugoton-USD-210.local>
2021-09-20 14:53:10 -0500

    fix img error in next

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -109,10 +109,13 @@ export default function Sidebar({ children }) {
               </Transition.Child>
               <div className="flex-1 h-0 pt-5 pb-4 overflow-y-auto">
                 <div className="flex-shrink-0 flex items-center px-4">
-                  <img
+                  <Image
                     className="h-8 w-auto"
                     src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg"
                     alt="Workflow"
+                    width={128}
+                    height={32}
+                    layout="intrinsic"
                   />
                 </div>
                 <nav className="mt-5 px-2 space-y-1">
@@ -142,25 +145,52 @@ export default function Sidebar({ children }) {
                 </nav>
               </div>
               <div className="flex-shrink-0 flex border-t border-gray-200 p-4">
-                <a href="#" className="flex-shrink-0 group block">
-                  <div className="flex items-center">
-                    <div>
-                      <img
-                        className="inline-block h-10 w-10 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                      />
-                    </div>
-                    <div className="ml-3">
-                      <p className="text-base font-medium text-gray-700 group-hover:text-gray-900">
-                        Tom Cook
-                      </p>
-                      <p className="text-sm font-medium text-gray-500 group-hover:text-gray-700">
-                        View profile
-                      </p>
-                    </div>
-                  </div>
-                </a>
+                {amplifyUser.state.user ? (
+                  <Link href="/account">
+                    <a href="#" className="flex-shrink-0 w-full group block">
+                      <div className="flex items-center">
+                        <div>
+                          {amplifyUser.state.user.attributes.picture ? (
+                            <Image
+                              className="inline-block rounded-full"
+                              width={36}
+                              height={36}
+                              src={`${amplifyUser.state.user.attributes.picture}`}
+                              alt=""
+                            />
+                          ) : (
+                            <div className="bg-indigo-600 rounded-full h-9 w-9 flex items-center justify-center text-white">
+                              {amplifyUser.state.user.attributes.name
+                                .match(/\b\w/g)
+                                .join("")}
+                            </div>
+                          )}
+                        </div>
+
+                        <div className="ml-3">
+                          <p className="text-sm font-medium text-gray-700 group-hover:text-gray-900">
+                            {amplifyUser.state.user.attributes.name}
+                          </p>
+                          <p className="text-xs font-medium text-gray-500 group-hover:text-gray-700">
+                            View profile
+                          </p>
+                        </div>
+                      </div>
+                    </a>
+                  </Link>
+                ) : (
+                  <button
+                    onClick={() => Auth.federatedSignIn({ provider: "Google" })}
+                    type="button"
+                    className="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  >
+                    <PlusIcon
+                      className="-ml-1 mr-2 h-5 w-5"
+                      aria-hidden="true"
+                    />
+                    Sign In
+                  </button>
+                )}
               </div>
             </div>
           </Transition.Child>
@@ -177,10 +207,13 @@ export default function Sidebar({ children }) {
           <div className="flex flex-col h-0 flex-1 border-r border-gray-200 bg-white">
             <div className="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
               <div className="flex items-center flex-shrink-0 px-4">
-                <img
-                  className="h-8 w-auto"
+                <Image
+                  className="w-auto"
                   src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg"
                   alt="Workflow"
+                  width={128}
+                  height={32}
+                  layout="intrinsic"
                 />
               </div>
               <nav className="mt-5 flex-1 px-2 bg-white space-y-1">
